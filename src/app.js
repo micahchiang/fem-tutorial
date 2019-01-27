ec3e5b7a8e82845b9666c17953389042260be09c
Micah Chiang <mchiang@rentreporters.com>
2019-01-26 23:27:29 -0800

    reach router

--- src/app.js
+++ src/app.js
@@ -1,36 +1,23 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
 
 class App extends React.Component {
-    handleTitleClick() {
-        alert('you clicked the title');
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    );
+  }
+}
 
-    render() {
-        return React.createElement("div", {}, [
-            React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me!'),
-            React.createElement(Pet, {
-                name: "luna",
-                animal: "dog",
-                breed: "havanese"
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "bird",
-                breed: "cockatiel"
-            }),
-            React.createElement(Pet, {
-                name: "Doink",
-                animal: "cat",
-                breed: "mixed"
-            }),
-        ]);
-    }
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById("root"));
